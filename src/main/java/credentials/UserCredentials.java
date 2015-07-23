fcfd2e5ce9249d6a5368702dd2d1de36f33bff9f
Pankaj Tiwana <pankajtiwana@Pankajs-MacBook-Pro.local>
2015-07-23 03:51:37 -0400

    pushing for image upload changes

--- src/main/java/credentials/UserCredentials.java
+++ src/main/java/credentials/UserCredentials.java
@@ -73,7 +73,14 @@ public class UserCredentials extends HttpServlet {
         PrintWriter out = response.getWriter();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-
+if(con==null)
+{
+    out.write("not");
+    
+}
+else{
+    out.write("yes");
+}
         try {
             smt = con.createStatement();
 
